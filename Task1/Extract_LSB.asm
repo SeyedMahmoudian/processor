f96769bcf85891b6887d98efe77f8616d1977f6b
SeyedMahmoudian <amin.s.mahmoudian@gmail.com>
2018-04-15 14:20:20 -0400

    Task one is done task 2 underway almost done

---
+++ Task1/Extract_LSB.asm
@@ -0,0 +1,25 @@
+; Extract_LSB.asm
+
+; Author:
+; Student Number:
+; Date:
+;
+; Purpose:
+;
+; Preconditions:
+;
+;
+; Postcondition:
+;
+;
+;
+MASK    equ     %00001111
+
+Extract_LSB
+        anda    #MASK
+        rts
+
+
+;- ------------------------------------
+;     End Extract_LSB.asm             -
+;--------------------------------------
---
+++ Task1/Extract_MSB.asm
@@ -0,0 +1,25 @@
+; Extract_MSB.asm
+
+; Author:
+; Student Number:
+; Date:
+;
+; Purpose:
+;
+; Preconditions:
+;
+;
+; Postcondition:
+;
+;
+;
+Extract_MSB
+        lsra
+        lsra
+        lsra
+        lsra
+        rts
+
+;- ------------------------------------
+;     End Extract_MSB.asm             -
+;--------------------------------------
